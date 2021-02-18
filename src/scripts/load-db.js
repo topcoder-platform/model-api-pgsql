89cc94887ad00803d9e53bc8b845fb18727f5dc1
urwithat <atif.siddiqui2@topcoder.com>
2021-02-18 15:32:56 +0530

    loading data scripts

--- src/scripts/load-db.js
+++ src/scripts/load-db.js
@@ -3,9 +3,11 @@
  */
 const config = require('config')
 const models = require('../models')
+const { Entity } = require('../models')
 const logger = require('../helpers/logger')
+const entities = require('../../resources/entities')
 
-const initDB = async () => {
+const loadDB = async () => {
   if (process.env.NODE_ENV !== 'development') {
     logger.error('Database loading should be executed in development env')
     process.exit()
@@ -13,18 +15,26 @@ const initDB = async () => {
   await models.sequelize.dropSchema(config.DB_SCHEMA_NAME)
   await models.sequelize.createSchema(config.DB_SCHEMA_NAME)
   await models.sequelize.sync({ force: true })
+  for (const entity of entities) {
+    await Entity.create({
+      createdAt: new Date(),
+      id: entity.id,
+      name: entity.name,
+      description: entity.description
+    })
+  }
 }
 
 if (!module.parent) {
-  initDB().then(() => {
-    logger.info({ component: 'init-db', message: 'Database synced successfully' })
+  loadDB().then(() => {
+    logger.info({ component: 'load-db', message: 'Database loaded successfully' })
     process.exit()
   }).catch((e) => {
-    logger.logFullError(e, { component: 'init-db' })
+    logger.logFullError(e, { component: 'load-db' })
     process.exit(1)
   })
 }
 
 module.exports = {
-  initDB
+  loadDB
 }
